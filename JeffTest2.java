bb38b3d0ad8ec6ad9908c4cdf31e1c7711f5d7e4
JJFOISEL <153690101+JJFOISEL@users.noreply.github.com>
2023-12-16 09:51:07 -0500

    Update JeffTest2.java

--- JeffTest2.java
+++ JeffTest2.java
@@ -1,6 +1,10 @@
-public class JeffTest2 {
+public class jeffTest2 {
+    /**
+     * 
+     */
     public static void main (String []) args 
     {
+        System.out.print("Test number A");
         System.out.println("Test number 2");
     }
 }
