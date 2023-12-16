6a6e60f022ca857eeb866fb6a7fe41854f5d68d6
Sillyman65 <tjesl2@gmail.com>
2023-12-16 09:48:34 -0500

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,10 @@
+class Test
+{
+    public static void main(String [] args)
+    { 
+        System.out.println("My Name is Tim");
+        System.out.println("Carson Needs Coffee");
+        System.out.println("Our computers are not talking");
+        System.out.println("We can do this");
+    }
+} 
