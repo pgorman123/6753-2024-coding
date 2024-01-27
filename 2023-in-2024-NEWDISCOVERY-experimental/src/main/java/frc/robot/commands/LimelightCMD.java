2811aa056694b582552143abe1043fc74e74e769
JackP007 <108008469+JackP007@users.noreply.github.com>
2024-01-27 11:23:53 -0500

    add limelight experimental code

---
+++ 2023-in-2024-NEWDISCOVERY-experimental/src/main/java/frc/robot/commands/LimelightCMD.java
@@ -0,0 +1,34 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.LimelightSubsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+public class LimelightCMD extends Command {
+
+    public LimelightSubsystem limelight_Subsystem;
+
+    public LimelightCMD() {
+    }
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void execute() {
+        SmartDashboard.putNumber("Limelight X", limelight_Subsystem.x);
+        SmartDashboard.putNumber("Limelight Y", limelight_Subsystem.y);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
